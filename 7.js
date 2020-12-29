f1417c32e872abbdfc7971e7144c0453975063c1
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2020-12-29 11:52:44 +0300

    Add files via upload

--- 7.js
+++ 7.js
@@ -6,3 +6,25 @@ let str_number = '6';
 let bool_false = false;
 let bool_true = true;
 let empty = null;
+
+console.log(str + ' ' + number);
+// Число преобразовывается в строковый тип
+
+console.log(+bool_false);
+// Логический тип в число
+
+console.log(number + bool_true);
+// Логический тип + число = число
+
+console.log(+empty);
+// Null к числу
+
+console.log(Number(str + str_number))
+// Строку к nan
+
+console.log(str + str_number)
+// Те же переменные, но без преобразования к числу
+
+console.log (Number(str_number) + bool_true)
+// Строку к числу + логический тип = число
+
